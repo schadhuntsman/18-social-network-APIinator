965792ebba63b76331fff78a483c986c287251b2
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-08-29 09:51:14 -0700

    udpated api

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} =require('../../controllers/userController')
+
+//set up get and post for api/user
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser)
+
+    //set up GET one put and delete at api pizza id
+
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+    module.exports = router;

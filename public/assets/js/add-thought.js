bd7c9238beb69fd3c67eba1c091e32b8027a34c1
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-08-30 23:47:45 -0700

    updated

--- public/assets/js/add-thought.js
+++ public/assets/js/add-thought.js
@@ -1,17 +1,35 @@
-const handleThoughtSubmit = event => {
-    event.preventDefault();
-  
-    const thoughtText = $thoughtForm.querySelector('#thought-text').value;
-    const createdAt = $thoughtForm.querySelector('#createdAt').value;
-    const thoughtName = $thoughtForm.querySelector('#thought-name').value;
-    const reactions = [...$thoughtForm.querySelectorAll('[text=reactions]:checked')].map(reactions => {
-      return reactions.value;
-    });
-  
-    if (!thoughtText || !createdAt || !thoughtName.length || reactions) {
-      return;
-    }
-  
-    const formData = { thoughtText, createdAt, thoughtName, reactions };
-  };
+function handleNewThoughtSubmit(event) {
+  event.preventDefault();
+
+    const thoughtText = $newThoughtForm.querySelector('#thought').value;
+    const username = $newThoughtForm.querySelector('#username').value;
+
+    if (!thoughtText || username)
+    return false;
+
+    const formData = { thoughtText, username };
+
+    fetch(`/api/${thought-routes}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(formData)
+    })
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(thoughtResponse => {
+      console.log(thoughtResponse);
+      locatoin.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
   
---
+++ public/assets/js/single-thought.js
@@ -0,0 +1,23 @@
+function getThought() {
+    //get id of thought
+    const thoughtId = searchParams.get('id');   
+    
+    //get thought info
+    fetch(`/api/thought-routes/${thoughtId}`)
+    .then(response => {
+        //check for a 4xx or 5xx error from server
+        if(!response.ok) {
+            throw new Error({ message: 'Something went wrong!' });
+        } 
+        
+        return response.json();
+    })
+    .then(printThought)
+    .catch(err => {
+        console.log(err);
+        alert('Cannot find a thought with this id! Taking you back.');
+        window.history.back();
+    });
+}
+
+getThought();

ce95c2b239ea0beb59b35186b53e9eba48eede1c
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-09-15 23:47:37 -0700

    updated

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+
+const app = express();
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('herro testing world')
 })
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipe-hunt', {
+    useFindAndModify: false,
     useNewParser: true,
     useUnifiedTopology: true,
 });
-//use this to log mongo queries being executed
+
 mongoose.set('debug', true);
 
 app.use(require('./api/routes'));
@@ -20,5 +25,5 @@ app.listen(port, () => {
     console.log(`${port}`)
 })
 
-app.listen(PORT, () => console.log(`connected to localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
 

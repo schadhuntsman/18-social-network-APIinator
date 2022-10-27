aabc346d61be8077a798c5920ddd7d8000c0410d
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-10-26 17:26:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.send('herro testing world')
+    res.send('hellooo testing world')
 })
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipe-hunt', {
-    useUnifiedTopology: true,
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-APIinator', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.use(require('./routes'));
 
-app.listen(PORT, () => {
-    console.log(`${PORT}`)
-})
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 
 

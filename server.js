a8c01c886d6e491ac68984ee967af11d0f5df958
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-10-26 18:41:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
-app.get('/', (req, res) => {
-    res.send('hellooo testing world')
-})
+// app.get('/', (req, res) => {
+//     res.send('hellooo testing world')
+// })
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-APIinator', {
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
